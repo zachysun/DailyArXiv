f0ba4c59e662f8763833f9ec638e753e750632ed
SUNZhengke <1754976743@qq.com>
2024-05-31 16:06:10 +0800

    final test

--- main.py
+++ main.py
@@ -40,6 +40,10 @@ f_rm = open("README.md", "w") # file for README.md
 f_rm.write("# Daily Papers\n")
 f_rm.write("The project automatically fetches the latest papers from arXiv based on keywords.\n\nThe subheadings in the README file represent the search keywords.\n\nOnly the most recent articles for each keyword are retained, up to a maximum of 100 papers.\n\nYou can click the 'Watch' button to receive daily email notifications.\n\nLast update: {0}\n\n".format(current_date))
 
+f_rm.write("## Index\n\n")
+for keyword in keywords:
+    f_rm.write("- [{0}](#{1})\n".format(keyword, keyword.replace(" ", "-")))
+
 # write to ISSUE_TEMPLATE.md
 f_is = open(".github/ISSUE_TEMPLATE.md", "w") # file for ISSUE_TEMPLATE.md
 f_is.write("---\n")
@@ -50,6 +54,7 @@ f_is.write("---\n")
 for keyword in keywords:
     f_rm.write("## {0}\n".format(keyword))
     f_is.write("## {0}\n".format(keyword))
+    f_rm.write("[Back to Index](#Index)\n\n")
     if len(keyword.split()) == 1: link = "AND" # for keyword with only one word, We search for papers containing this keyword in both the title and abstract.
     else: link = "OR"
     papers = get_daily_papers_by_keyword_with_retries(keyword, column_names, max_result, link)
