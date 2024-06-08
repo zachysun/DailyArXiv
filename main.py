8b850f9b129c7e4e8e91d62a67e6f60e6c76ded2
ZHENGKE SUN <74416252+zachysun@users.noreply.github.com>
2024-06-08 18:08:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ keywords = ["Time Series", "LLM", "Image Restoration",
             "Diffusion Model", "Vision Language Model", 
             "Photoacoustic Image"] # TODO add more keywords
 
-max_result = 50 # maximum query results from arXiv API for each keyword
-issues_result = 15 # maximum papers to be included in the issue
+max_result = 100 # maximum query results from arXiv API for each keyword
+issues_result = 20 # maximum papers to be included in the issue
 
 # all columns: Title, Authors, Abstract, Link, Tags, Comment, Date
 # fixed_columns = ["Title", "Link", "Date"]
